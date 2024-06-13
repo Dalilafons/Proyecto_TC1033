b8fb31d0da2e1ec20fd081bb3f80816f5486cd4e
Dalilafons <142357126+Dalilafons@users.noreply.github.com>
2024-06-12 20:50:07 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 /*
- * Proyecto Tienda main
+ * Proyecto Tienda main 
  * Dalila Fonseca Maya
  * A01711722
  * 06/06/2024
  *
  * Este es un proyecto para la clase TC1030 Programación Orientado a
- * Objetos. Es un programa que captura diferentes tipos de productos (Alimentos y Artículos),
- * los gestiona en un inventario y permite realizar ventas calculando los precios
- * con polimorfismo.
+ * Objetos. Es un programa que captura diferentes tipos de 
+ * productos (Alimentos y Artículos),los gestiona en un inventario y 
+ * permite realizar ventas calculando los precios con polimorfismo.
  */
 
 #include <iostream>  
@@ -30,8 +30,10 @@ int main(void)
     Producto* producto2 = new Articulo(2, "Laptop", 1000.0, 5, 15.0);
 
     // Mostrar precio:
-    std::cout << "Precio del " << producto1->getNombre() << " : " << producto1->calcularPrecio() << std::endl;
-    std::cout << "Precio de la " << producto2->getNombre() << " : " << producto2->calcularPrecio() << std::endl;
+    std::cout << "Precio del " << producto1->getNombre() << " : " 
+    << producto1->calcularPrecio() << std::endl;
+    std::cout << "Precio de la " << producto2->getNombre() << " : " 
+    << producto2->calcularPrecio() << std::endl;
 
     // Crear inventario y agregar productos:
     Inventario inventario;
@@ -46,7 +48,8 @@ int main(void)
     std::cout << "Buscando producto con ID 1:" << std::endl;
     if (Producto* productoEncontrado = inventario.buscarProducto(1)) {
         std::cout << "Producto encontrado: " << productoEncontrado->getNombre() 
-                  << ", Precio calculado: " << productoEncontrado->calcularPrecio() << std::endl;
+        << ", Precio calculado: " << productoEncontrado->calcularPrecio() 
+        << std::endl;
     }
 
     // Crear una venta y agregar productos vendidos:
@@ -60,7 +63,8 @@ int main(void)
 
     // Calcular total de la venta con y sin descuento usando polimorfismo
     std::cout << "Total de la venta: " << venta.calcularTotal() << std::endl;
-    std::cout << "Total de la venta con 10% de descuento: " << venta.calcularTotal(10.0) << std::endl;
+    std::cout << "Total de la venta con 10% de descuento: " 
+    << venta.calcularTotal(10.0) << std::endl;
 
     //Crear Tienda:
     Tienda tienda;
