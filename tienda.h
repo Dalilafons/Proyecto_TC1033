c520857b3833feb9ae9f0ce891886a4f59a80a3f
Dalilafons <142357126+Dalilafons@users.noreply.github.com>
2024-06-12 20:48:59 -0600

    Update tienda.h

--- tienda.h
+++ tienda.h
@@ -1,13 +1,14 @@
 /*
  *
- * Proyecto Tienda clase Tienda
+ * Proyecto Tienda clase Tienda 1
  * Dalila Fonseca Maya
  * A01711722
  * 06/06/2024
  * 
- * Esta clase define un objeto de tipo Tienda que gestiona el inventario y las ventas.
- * Permite crear y agregar productos al inventario, realizar ventas de productos, 
- * y mostrar la información tanto del inventario como de las ventas realizadas.
+ * Esta clase define un objeto de tipo Tienda que gestiona el inventario 
+ * y las ventas.Permite crear y agregar productos al inventario, 
+ * realizar ventas de productos y mostrar la información tanto del 
+ * inventario como de las ventas realizadas.
  * Utiliza polimorfismo para manejar productos de diferentes tipos.
  * Esta clase es parte del proyecto Tienda.
  */
@@ -52,7 +53,8 @@ Tienda::Tienda(){}
 /**
  * crearInventario crea un inventario de productos.
  *
- * Solicita al usuario la cantidad de productos y los detalles de cada producto, y los agrega al inventario.
+ * Solicita al usuario la cantidad de productos y 
+ * los detalles de cada producto, y los agrega al inventario.
  * 
  * @param
  * @return
@@ -88,13 +90,15 @@ void Tienda::crearInventario()
 
                 if (tipoProducto == 1)
                 {
-                    inventario.agregarProducto(new Alimento(id, nombre, precio, cantidad));
+                inventario.agregarProducto(new Alimento(id, nombre, 
+                precio, cantidad));
                 }
                 else if (tipoProducto == 2)
                 {
                     std::cout << "Tasa de Impuesto (%): ";
                     std::cin >> tasaImpuesto;
-                    inventario.agregarProducto(new Articulo(id, nombre, precio, cantidad, tasaImpuesto));
+                    inventario.agregarProducto(new Articulo(id, nombre, 
+                    precio, cantidad, tasaImpuesto));
                 }
                 else
                 {
@@ -113,8 +117,9 @@ void Tienda::crearInventario()
 /**
  * crearVenta crea una nueva venta.
  *
- * Solicita al usuario la cantidad de productos a agregar a la venta y los detalles de cada producto.
- * Luego, agrega los productos a la venta si se encuentran en el inventario.
+ * Solicita al usuario la cantidad de productos a agregar a la venta 
+ * y los detalles de cada producto. Luego, agrega los productos a la 
+ * venta si se encuentran en el inventario.
  * 
  * @param
  * @return
@@ -134,18 +139,21 @@ void Tienda::crearVenta()
             for (int i = 0; i < numProductos; ++i)
             {
                 int id, cantidad;
-                std::cout << "Agregar producto " << i + 1 << " a la venta:" << std::endl;
+                std::cout << "Agregar producto " << i + 1 
+                 << " a la venta:" << std::endl;
                 std::cout << "ID: ";
                 std::cin >> id;
                 Producto* producto = inventario.buscarProducto(id);
                 if (producto != nullptr)
                 {
                     ventaActual.agregarProductoVenta(producto);
-                    std::cout << "Producto agregado a la venta." << std::endl;
+                    std::cout << "Producto agregado a la venta." 
+                    << std::endl;
                 }
                 else
                 {
-                    std::cout << "Producto no encontrado en el inventario." << std::endl;
+                    std::cout << "Producto no encontrado en el inventario." 
+                    << std::endl;
                 }
             }
             std::cout << "Venta creada." << std::endl;
@@ -159,7 +167,8 @@ void Tienda::crearVenta()
 /**
  * mostrarInformacionInventario muestra la información del inventario.
  *
- * Llama al método mostrarInventario del objeto Inventario para imprimir la información de todos los productos en el inventario.
+ * Llama al método mostrarInventario del objeto Inventario para 
+ * imprimir la información de todos los productos en el inventario.
  * 
  * @param
  * @return
@@ -172,7 +181,9 @@ void Tienda::mostrarInformacionInventario()
 /**
  * mostrarInformacionVenta muestra la información de la venta.
  *
- * Llama al método mostrarDetallesVenta del objeto Venta para imprimir la información de todos los productos vendidos y el total de la venta.
+ * Llama al método mostrarDetallesVenta del objeto Venta para 
+ * imprimir la información de todos los productos vendidos 
+ * y el total de la venta.
  * 
  * @param 
  * @return
